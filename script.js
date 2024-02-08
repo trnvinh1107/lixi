336e5dbb51e4ebab30eb61584db9f7ccfbf5830d
trnvinh1107 <vinhtranthe117@gmail.com>
2024-02-08 22:18:03 +0700

    abc

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var count = 0;
 document.getElementById("btn-cook").addEventListener("mouseover", function () {
+  var img1 = document.getElementById("img1");
+  var img2 = document.getElementById("img2");
+  var btnOke = document.getElementById("btn-oke");
+  var btnCook = document.getElementById("btn-cook");
   if (count != 0) {
-    document.getElementById("img2").style.display = "none";
-    document.getElementById("img1").style.display = "block";
-    document.getElementById("btn-cook").style.transform += "translateX(-90px)";
-    document.getElementById("btn-cook").style.transform += "translateY(-50px)";
-    document.getElementById("btn-oke").style.transform = "translateX(130px)";
-    var btnOke = document.getElementById("btn-oke");
+    btnCook.style.transform += "translateX(-90px)";
+    btnCook.style.transform += "translateY(-50px)";
+    btnOke.style.transform = "translateX(130px)";
     // Lấy chiều cao hiện tại của button
     var currentHeight = btnOke.offsetHeight;
     var currentWidth = btnOke.offsetWidth;
@@ -15,32 +16,40 @@ document.getElementById("btn-cook").addEventListener("mouseover", function () {
     btnOke.style.top = currentTop - 25 + "%";
     btnOke.style.height = currentHeight + 20 + "px";
     btnOke.style.width = currentWidth + 20 + "px";
+    //btn-cook
+    // Lấy chiều cao hiện tại của button
+    var currentHeight_cook = btnCook.offsetHeight;
+    var currentWidth_cook = btnCook.offsetWidth;
+    // Tăng chiều cao thêm 30px
+    btnCook.style.height = currentHeight_cook - 5 + "px";
+    if (count > 1) btnCook.style.width = currentWidth_cook - 20 + "px";
+    // else btnCook.style.width = currentWidth_cook - 20 + "px";
+    var currentFontSize = parseFloat(window.getComputedStyle(this).fontSize); // Get the current font size as a number
+    var newFontSize = currentFontSize - 5; // Decrease the font size by 10px
+    btnCook.style.fontSize = newFontSize + "px";
   } else {
-    var btnOke = document.getElementById("btn-oke");
-    var btnCook = document.getElementById("btn-cook");
     var parentElement = btnOke.parentElement;
-
-    // Lưu trữ vị trí của btnCook1 trong parentElement
     var temp = document.createElement("div");
     parentElement.insertBefore(temp, btnOke);
-
-    // Di chuyển btnCook2 đến vị trí của btnCook1
     parentElement.insertBefore(btnCook, temp);
-
-    // Di chuyển btnCook1 đến vị trí của btnCook2
     parentElement.insertBefore(btnOke, btnCook);
-
-    // Xóa phần tử tạm thời
     parentElement.removeChild(temp);
     btnCook.style.marginRight = "0";
     btnOke.style.marginRight = "35%";
   }
   count++;
+  console.log(count);
 });
 document.getElementById("btn-oke").addEventListener("mouseover", function () {
   if (count != 1) {
     document.getElementById("img1").style.display = "none";
     document.getElementById("img2").style.display = "block";
+  }
+});
+document.getElementById("btn-oke").addEventListener("mouseout", function () {
+  if (count != 1) {
+    document.getElementById("img1").style.display = "block";
+    document.getElementById("img2").style.display = "none";
   }
 });
 document.getElementById("btn-oke").addEventListener("click", function () {
